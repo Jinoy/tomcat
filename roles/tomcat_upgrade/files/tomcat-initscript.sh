262e339856f781858204d8c6f4da2fe33748dd73
root <root@acs>
2018-03-18 15:56:47 +0000

    sonar files added

--- roles/tomcat_upgrade/files/tomcat-initscript.sh
+++ roles/tomcat_upgrade/files/tomcat-initscript.sh
@@ -3,9 +3,7 @@
 # chkconfig: 345 99 28
 # description: Starts/Stops Apache Tomcat
 #
-# Tomcat 7 start/stop/status script
-# Forked from: https://gist.github.com/valotas/1000094
-# @author: Miglen Evlogiev <bash@miglen.com>
+# Tomcat 8 start/stop/status script
 #
 # Release updates:
 # Updated method for gathering pid of the current proccess
